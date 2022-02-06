63d5f5416a5907cb8c5aa3e4cf4487b43ccdf5aa
hoomanti <manisha.edu@protonmail.com>
2022-02-06 11:33:53 +0530

    Repeat till position reaches 100, Also position below 0 will restart from 0

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,28 +1,37 @@
 public class SnakeLadder {
+
 	static int position = 0;
 	public static void main(String args[]) {
-		int roll = (int)(Math.floor(Math.random() * 10 ) % 6 ) +1;
-		System.out.println("ROLL = " + roll);
 
-		int option = (int)(Math.floor(Math.random() * 10 ) % 3 ) + 1;
-		switch (option) {
-		case 1:
-			System.out.println("NO PLAY, STAY AT SAME POSITION");
-			System.out.println("Your position = " + position);
-			break;
-		case 2:
-			System.out.println("CLIMB THE LADDER");
-			position = position + roll + roll;
-			System.out.println("Your position = " + position);
-			break;
-		case 3:
-			System.out.println("SNAKE BITE");
-			position = position - roll;
-			System.out.println("Your position = " + position);
-			break;
-		default:
-			System.out.println("INVALID MOVE");
-			break;
+		while(position < 100) {
+			int roll = (int)(Math.floor(Math.random() * 10 ) % 6 ) +1;
+			System.out.println("ROLL = " + roll);
+
+			int option = (int)(Math.floor(Math.random() * 10 ) % 3 ) + 1;
+			switch (option) {
+			case 1:
+				System.out.println("NO PLAY, STAY AT SAME POSITION");
+				System.out.println("Your position = " + position);
+				break;
+			case 2:
+				System.out.println("CLIMB THE LADDER");
+				position = position + roll + roll;
+				System.out.println("Your position = " + position);
+				break;
+			case 3:
+				System.out.println("SNAKE BITE");
+				position = position - roll;
+				System.out.println("Your position = " + position);
+				break;
+			default:
+				System.out.println("INVALID MOVE");
+				break;
+			}
+
+			if(position < 0) {
+				position = 0;
+				System.out.println("Start Again");
+			}
 		}
 	}
 }
