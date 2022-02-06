30099d2286d21c5b2844b00fde9c44e0d87cd9c1
hoomanti <manisha.edu@protonmail.com>
2022-02-06 10:50:36 +0530

    Rolling dice to get a number between 1-6

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,6 +1,9 @@
 public class SnakeLadder {
 	static int position = 0;
 	public static void main(String args[]) {
+		int roll = (int)(Math.floor(Math.random() * 10 ) % 6 ) +1;
+		System.out.println("ROLL = " + roll);
+		position = position + roll;
 		System.out.println("Your position = " + position);
 	}
 }
