ac3322839c8109e9b6a457250d9051b826b364a1
hoomanti <manisha.edu@protonmail.com>
2022-02-06 10:41:35 +0530

    Starting position = 0

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,5 +1,6 @@
 public class SnakeLadder {
+	static int position = 0;
 	public static void main(String args[]) {
-		System.out.println("Welcome to Snake & Ladder simulator");
+		System.out.println("Your position = " + position);
 	}
 }
