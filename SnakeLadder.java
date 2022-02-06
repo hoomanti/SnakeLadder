dfe2ff1378b25f01b1e5753f0e36e44cbdfb7c10
hoomanti <manisha.edu@protonmail.com>
2022-02-06 11:09:31 +0530

    Check option for NO PLAY, LADDER, SNAKE

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -3,7 +3,26 @@ public class SnakeLadder {
 	public static void main(String args[]) {
 		int roll = (int)(Math.floor(Math.random() * 10 ) % 6 ) +1;
 		System.out.println("ROLL = " + roll);
-		position = position + roll;
-		System.out.println("Your position = " + position);
+
+		int option = (int)(Math.floor(Math.random() * 10 ) % 3 ) + 1;
+		switch (option) {
+		case 1:
+			System.out.println("NO PLAY, STAY AT SAME POSITION");
+			System.out.println("Your position = " + position);
+			break;
+		case 2:
+			System.out.println("CLIMB THE LADDER");
+			position = position + roll + roll;
+			System.out.println("Your position = " + position);
+			break;
+		case 3:
+			System.out.println("SNAKE BITE");
+			position = position - roll;
+			System.out.println("Your position = " + position);
+			break;
+		default:
+			System.out.println("INVALID MOVE");
+			break;
+		}
 	}
 }
